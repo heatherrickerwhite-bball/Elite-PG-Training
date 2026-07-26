9e43a1c6a293db83dd07e5b01cff7b6fb752d916
heatherrickerwhite-bball <heather.ricker.white@gmail.com>
2026-07-25 21:25:10 -0400

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,23 @@
+const Storage = {
 
+save(key,value){
+
+localStorage.setItem(key,JSON.stringify(value));
+
+},
+
+load(key){
+
+const data=localStorage.getItem(key);
+
+return data ? JSON.parse(data) : null;
+
+},
+
+remove(key){
+
+localStorage.removeItem(key);
+
+}
+
+};
