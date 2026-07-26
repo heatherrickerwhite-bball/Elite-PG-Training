324010c314edb9ef450ed0e24f43b6c8b47ce54a
heatherrickerwhite-bball <heather.ricker.white@gmail.com>
2026-07-25 21:26:09 -0400

    Update streak.js

--- js/streak.js
+++ js/streak.js
@@ -1,1 +1,19 @@
+const today = new Date().toDateString();
 
+const lastWorkout = Storage.load("lastWorkout");
+
+let streak = Storage.load("streak") || 0;
+
+function completeWorkout(){
+
+if(lastWorkout !== today){
+
+streak++;
+
+Storage.save("streak",streak);
+
+Storage.save("lastWorkout",today);
+
+}
+
+}
